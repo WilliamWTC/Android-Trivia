2c9f684c551cd88467bdd435c47b80df9fecc921
Lesiba <lesiba.mabitsela@fnb.co.za>
2021-08-12 12:51:47 +0200

    2nd Commit.

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -1,29 +1,148 @@
 package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.trivia.controller.AppController;
 import com.example.trivia.data.AnswerListAsyncResponse;
 import com.example.trivia.data.Repository;
+import com.example.trivia.databinding.ActivityMainBinding;
 import com.example.trivia.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
+    List<Question> questions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Question> questions = new Repository().getQuestions(questionArrayList ->
-                Log.d("Main", "" + questionArrayList));
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        questions = new Repository().getQuestions(questionArrayList -> {
+
+                binding.questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+
+                updateCounter(questionArrayList);
+                }
+                );
+
+        binding.buttonNext.setOnClickListener(v -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questions.size();
+            updateQuestion();
+        });
+        binding.buttonBack.setOnClickListener(v -> {
+            if (currentQuestionIndex > 0) {
+                currentQuestionIndex = (currentQuestionIndex - 1) % questions.size();
+            } else {
+                currentQuestionIndex = 0;
+            }
+            previousQuestion();
+        });
+        binding.buttonTrue.setOnClickListener(v -> {
+            checkAnswer(true);
+            updateQuestion();
+        });
+        binding.buttonFalse.setOnClickListener(v -> {
+            checkAnswer(false);
+            updateQuestion();
+        });
 
     }
+
+    private void checkAnswer(boolean usersChoice) {
+        boolean answer = questions.get(currentQuestionIndex).isAnswerTrue();
+        int snackMessageId = 0;
+        if (usersChoice == answer){
+            snackMessageId = R.string.correct_answer;
+            fadeAnimation();
+        }else {
+            snackMessageId = R.string.incorrect_answer;
+            shakeAnimation();
+        }
+
+        Snackbar.make(binding.cardView, snackMessageId, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void updateCounter(ArrayList<Question> questionArrayList) {
+        binding.textViewOutOf.setText(String.format(getString(R.string.text_formatted), currentQuestionIndex, questionArrayList.size()));
+    }
+
+    private void previousQuestion() {
+        String question = questions.get(currentQuestionIndex).getAnswer();
+        binding.questionTextview.setText(question);
+        updateCounter((ArrayList<Question>) questions);
+    }
+
+    private void updateQuestion() {
+        String question = questions.get(currentQuestionIndex).getAnswer();
+        binding.questionTextview.setText(question);
+        updateCounter((ArrayList<Question>) questions);
+    }
+
+    private void fadeAnimation() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+        alphaAnimation.setDuration(300);
+        alphaAnimation.setRepeatCount(1);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+
+        binding.cardView.setAnimation(alphaAnimation);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextview.setTextColor(Color.GREEN);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextview.setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private void shakeAnimation() {
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this,
+                R.anim.shake_anim);
+        binding.cardView.setAnimation(shake);
+
+        shake.setAnimationListener((new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextview.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextview.setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        }));
+    }
+
 }
